cb1ea2a3136effcb6eb9a2dd5b8385ef5cfca1c5
dhrxv03 <dhruv0310@icloud.com>
2024-11-06 23:42:48 +0530

    top logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import logo from './assets/logo.svg';
+import logoT from './assets/logo2.svg';
 import './App.css';
 import MouseTrail from './MouseTrail';
 
@@ -18,17 +19,21 @@ function App() {
         <img src={logo} alt="Company Logo" className="logo" />
       </div>
 
+      <div className="logoT">
+        <img src={logoT} alt="logoTop" className="logoT" />
+      </div>
+
       <div className="main-content">
         <h1>ACCENTS · OBJECTS · LIFESTYLE</h1>
         <p>We are crafting something special. <br /> Till then connect to us through our socials.</p>
-
+    
         <div className="footer">
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a href="https://www.instagram.com/olkap.official/" target="_blank" rel="noopener noreferrer" className="social-link">
             <FontAwesomeIcon icon={faInstagram} className="social-icon" />
             <span>Instagram</span>
           </a>
           <span className="address-text">Tower 1, City Point, Boat Club Rd, Pune - 411001</span>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a href="https://www.linkedin.com/company/olkapofficial/" target="_blank" rel="noopener noreferrer" className="social-link">
             <FontAwesomeIcon icon={faLinkedin} className="social-icon" />
             <span>LinkedIn</span>
           </a>
